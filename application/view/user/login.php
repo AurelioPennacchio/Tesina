b569134c1a014cc8f7e0837969cc15588abad4b4
Aurelio <Sandra@Air-di-Sandra.homenet.telecomitalia.it>
2017-03-31 16:18:54 +0200

    Sistemato il form di login con aggiunta di librerie css e javascript

--- application/view/user/login.php
+++ application/view/user/login.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title><?php echo $title; ?></title>
+	<script type="text/javascript" src="<?php echo URL; ?>js/jquery.js"></script>
 	<link rel="stylesheet" href="<?php echo URL; ?>css/pure-min.css">
 	<link rel="stylesheet" href="<?php echo URL; ?>css/style.css">
 	<link rel="stylesheet" href="<?php echo URL; ?>css/grids-responsive-min.css">
@@ -9,12 +10,12 @@
 </head>
 <body>
 	<div class="container">
-		<form class="pure-form contenitore">
+		<form class="pure-form contenitore" method="POST" action="<?php echo URL; ?>user/login">
 		    <fieldset class="pure-group">
-		    	<input type="text" class="pure-input-1-2" placeholder="Email">
-		        <input type="text" class="pure-input-1-2" placeholder="Password">
+		    	<input type="text" class="pure-input-1-2" placeholder="Email" name="email">
+		        <input type="password" class="pure-input-1-2" placeholder="Password" name="password">
 		    </fieldset>
-		    <button type="submit" class="pure-button pure-input-1-2 pure-button-primary">Login</button>
+		    <button type="submit" class="pure-button pure-input-1-2 pure-button-primary" name="login" >Login</button>
 		    <br>
 		    <a href="<?php echo URL; ?>user/registration">Not registered? Click here</a>
 		</form>
