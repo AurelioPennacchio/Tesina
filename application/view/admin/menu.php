eff8df21160518876fedec24b99ee55767bbca27
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.homenet.telecomitalia.it>
2017-04-15 18:51:17 +0200

    Modifiche nel menu

--- application/view/admin/menu.php
+++ application/view/admin/menu.php
@@ -12,19 +12,16 @@
 	<div class="pure-menu pure-menu-horizontal">
 		<ul class="pure-menu-list">
 			<li class="pure-menu-item">
-				<a href="<?php echo URL; ?>admin/aggiungistudente" class="pure-button">Aggiungi cibo</a>
+				<a href="<?php echo URL; ?>admin/aggiungiCibo" class="pure-button">Aggiungi cibo</a>
 			</li>
 			<li class="pure-menu-item">
-				<a href="<?php echo URL; ?>admin/classifica" class="pure-button">Elimina cibo</a>
+				<a href="<?php echo URL; ?>admin/eliminaCibo" class="pure-button">Elimina cibo</a>
 			</li>
 			<li class="pure-menu-item">
-				<a href="<?php echo URL; ?>admin/classificaSquadre/allieviM" class="pure-button">Vedi prenotazioni</a>
+				<a href="<?php echo URL; ?>admin/vediPrenotazioni" class="pure-button">Vedi prenotazioni</a>
 			</li>
 			<li class="pure-menu-item">
-				<a href="<?php echo URL; ?>admin/generaPettorine" class="pure-button">Statistiche</a>
-			</li>
-			<li class="pure-menu-item">
-				<a href="<?php echo URL; ?>admin/elenco" class="pure-button ">Logout</a>
+				<a href="<?php echo URL; ?>admin/logout" class="pure-button ">Logout</a>
 			</li>
 		</ul>
 	</div>
