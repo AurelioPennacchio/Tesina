e67e92bbf32faf9e6398f9ac159c2b1618c334e8
Aurelio <Sandra@Air-di-Sandra.homenet.telecomitalia.it>
2017-05-15 16:58:58 +0200

    Sistemato il controllo password

--- application/controller/user.php
+++ application/controller/user.php
@@ -31,7 +31,7 @@ class User extends Controller
 			header('location:' . URL . 'user/home');
 		}
 		if(isset($_POST['email']) && isset($_POST['password'])){
-			$result = $this->model->findUser($_POST['email']);
+			$result = $this->model->findUser($_POST['email'], $_POST['password']);
 			//funziona con le cose che ritorna
 			if($result == false){
 				$info = 'Utente non registrato o dati inseriti errati';
