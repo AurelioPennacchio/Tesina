b2110b8ab26e753fadfd4ed48be14ad00cc03503
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.homenet.telecomitalia.it>
2017-05-21 17:45:54 +0200

    Modifica della pagina

--- application/view/user/informazioni.php
+++ application/view/user/informazioni.php
@@ -1,5 +1,9 @@
 
-			<h1>Ecco tutte le informazioni</h1>
+			
+			<div>
+				<h1>Ecco tutte le informazioni</h1>
+				
+			</div>
 		</div>
 	</body>
 	<script type="text/javascript" src="<?php echo URL; ?>js/ui.js"></script>
