e93887eebf11e640beff4cb000329f959b43cf5d
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.homenet.telecomitalia.it>
2017-05-21 17:58:39 +0200

    Ora vengono stampate le info dello user

--- application/view/user/informazioni.php
+++ application/view/user/informazioni.php
@@ -2,7 +2,10 @@
 			
 			<div>
 				<h1>Ecco tutte le informazioni</h1>
-				
+				<p>Nome: <?php echo $informazioni->nome; ?></p>
+				<p>Cognome: <?php echo $informazioni->cognome; ?></p>
+				<p>Email: <?php echo $informazioni->email; ?></p>
+				<p>Data di nascita: <?php echo $informazioni->data_nascita; ?></p>
 			</div>
 		</div>
 	</body>
