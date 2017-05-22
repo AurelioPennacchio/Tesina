ec2cb445a2daf60755a1cbb802aac22d9150c902
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.local>
2017-05-22 15:42:24 +0200

    Aggiunto menù per prenotare

--- application/view/user/home.php
+++ application/view/user/home.php
@@ -1,5 +1,25 @@
 
 			<h1>Pagina di prenotazione</h1>
+			<div>
+				<div>
+					<form class="pure-form pure-form-stacked">
+						<fieldset>
+							<label>Primo piatto</label>
+							<select name="primo_piatto">
+								
+							</select>
+							<label>Secondo piatto</label>
+							<select name="secondo_piatto">
+								
+							</select>
+							<label>Bibita</label>
+							<select name="bibita">
+								
+							</select>
+						</fieldset>
+					</form>
+				</div>
+			</div>
 		</div>
 	</body>
 	<script type="text/javascript" src="<?php echo URL; ?>js/ui.js"></script>
