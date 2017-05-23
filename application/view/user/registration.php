b4c48e84dcfea4cc948fd498b555ed749be1427a
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.local>
2017-05-23 21:33:15 +0200

    Pagina adattata al responsive

--- application/view/user/registration.php
+++ application/view/user/registration.php
@@ -2,30 +2,60 @@
 <html>
 <head>
 	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="<?php echo URL; ?>css/materialize.css">
 	<script type="text/javascript" src="<?php echo URL; ?>js/jquery.js"></script>
-	<link rel="stylesheet" href="<?php echo URL; ?>css/pure-min.css">
-	<link rel="stylesheet" href="<?php echo URL; ?>css/style.css">
-	<link rel="stylesheet" href="<?php echo URL; ?>css/grids-responsive-min.css">
-	<link rel="stylesheet" href="<?php echo URL; ?>css/font-awesome/css/font-awesome.min.css" type="text/css">
+	<script type="text/javascript" src="<?php echo URL; ?>js/materialize.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
-	<div>
-		<form class="pure-form contenitore" id="form_registrazione" method="POST">
-		    <fieldset class="pure-group">
-		    	<input type="text" class="pure-input-1-2" placeholder="Nome" name="nome" required>
-		    	<input type="text" class="pure-input-1-2" placeholder="Cognome" name="cognome" required>
-		    	<input type="date" class="pure-input-1-2" placeholder="Data di nascita" name="data_nascita" required>
-		    	<input type="text" class="pure-input-1-2" placeholder="Email" name="email" required>
-		        <input type="password" class="pure-input-1-2" placeholder="Password" name="password" required>
-		        <input type="password" class="pure-input-1-2" placeholder="Confirm password" required>
-		    </fieldset>
-		    <button type="submit" class="pure-button pure-input-1-2 pure-button-primary" name="register">Register</button>
-		</form>
-		<a href="<?php echo URL; ?>/user/index">Login page</a>
+	
+	<div class="container">
+		<div class="row">
+			<form class="col s12" method="POST" id="form_registrazione">
+				<div class="row">
+					<div class="input-field col s12">
+						<input type="text" name="nome" placeholder="Nome" class="validate">
+						<label for="nome">Nome</label>
+					</div>
+				</div>
+				<div class="row">
+					<div class="input-field col s12">
+						<input type="text" name="cognome" placeholder="Cognome" class="validate">
+						<label for="cognome">Cognome</label>
+					</div>
+				</div>
+				<div class="row">
+					<div class="input-field col s12">
+						<label for="data_nascita">Data di nascita</label>
+						<input type="date" name="data_nascita" class="datepicker">
+					</div>
+				</div>
+				<div class="row">
+					<div class="input-field col s12">
+						<input type="text" name="email" placeholder="Email" class="validate">
+						<label for="email">Email</label>
+					</div>
+				</div>
+				<div class="row">
+					<div class="input-field col s12">
+						<input type="password" name="password" placeholder="Password" class="validate">
+						<label for="Password">Password</label>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col s6 offset-s3">
+						<button class="btn waves-effect waves-light" type="submit" name="login">Registrati</button>
+					</div>
+				</div>
+			</form>
+		</div>
 	</div>
-	<div id="prova">
-		
-	</div>
+	<script type="text/javascript">
+		$('.datepicker').pickadate({
+			selectMonths: true, // Creates a dropdown to control month
+			selectYears: 15 // Creates a dropdown of 15 years to control year
+		});
+	</script>
 	<script type="text/javascript">
 
 		$('#form_registrazione').submit(function(e) {
