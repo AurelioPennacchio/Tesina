df8a940b4edfc6ad11e34b9c74e5c77b592da70b
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.local>
2017-05-23 19:43:12 +0200

    Ora interfaccia material

--- application/view/user/login.php
+++ application/view/user/login.php
@@ -5,39 +5,38 @@
 <html>
 <head>
 	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="<?php echo URL; ?>css/materialize.css">
 	<script type="text/javascript" src="<?php echo URL; ?>js/jquery.js"></script>
-	<link rel="stylesheet" href="<?php echo URL; ?>css/pure-min.css">
-	<link rel="stylesheet" href="<?php echo URL; ?>css/style.css">
-	<link rel="stylesheet" href="<?php echo URL; ?>css/grids-responsive-min.css">
-	<link rel="stylesheet" href="<?php echo URL; ?>css/font-awesome/css/font-awesome.min.css" type="text/css">
-	<style type="text/css">
-		.button-error{
-			color: white;
-			border-radius: 4px;
-			text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
-			background: rgb(202, 60, 60);
-		}
-	</style>
+	<script type="text/javascript" src="<?php echo URL; ?>js/materialize.js"></script>
 </head>
 <body>
-	<div class="container">
-		<form class="pure-form contenitore" method="POST" action="<?php echo URL; ?>user/login">
-		    <fieldset class="pure-group">
-		    	<input type="text" class="pure-input-1-2" placeholder="Email" name="email">
-		        <input type="password" class="pure-input-1-2" placeholder="Password" name="password">
-		    </fieldset>
-		    <button type="submit" class="pure-button pure-input-1-2 pure-button-primary" name="login" >Login</button>
-		    <br>
-		    <br>
-		    <a class="pure-button pure-button-primary" href="<?php echo URL; ?>user/registration">Not registered? Click here</a>
-		    <br>
-		    <br>
-		    <?php 
+	
+	<div class="row">
+		<form class="col s12" method="POST" action="<?php echo URL; ?>user/login">
+			<div class="row">
+				<div class="input-field col s6">
+					<input type="text" name="email" id="email" placeholder="Email" class="validate">
+					<label for="email">Email</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s6">
+					<input type="password" name="password" id="password" placeholder="Password" class="validate">
+					<label for="password">Password</label>
+				</div>
+			</div>
+			<div class="row">
+				<button class="btn waves-effect waves-light" type="submit" name="login">Login</button>
+				<a class="waves-effect waves-light btn" href="<?php echo URL; ?>user/registration">Non registrato? Clicca qui</a>
+			</div>
+			<?php 
 				if(isset($info)){
-					echo "<button class=\"button-error pure-button\">$info</button>";
+					echo "<div class=\"row\">";
+					echo "<a class=\"waves-effect waves-light btn\">$info</a>";
+					echo "</div>";
 				}
 			?>
 		</form>
-	</div>
+	</div>	
 </body>
 </html>
