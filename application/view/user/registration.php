6010bed5f142c158b73b172a90a4c0fca2cd8b89
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.local>
2017-05-23 21:36:28 +0200

    Apportate alcune modifiche alla pagina

--- application/view/user/registration.php
+++ application/view/user/registration.php
@@ -53,7 +53,7 @@
 	<script type="text/javascript">
 		$('.datepicker').pickadate({
 			selectMonths: true, // Creates a dropdown to control month
-			selectYears: 15 // Creates a dropdown of 15 years to control year
+			selectYears: 150 // Creates a dropdown of 15 years to control year
 		});
 	</script>
 	<script type="text/javascript">
