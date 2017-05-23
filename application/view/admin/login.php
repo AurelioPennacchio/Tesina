ee01f2dfe5ff07a9166090ace5a69c1c13a1ba40
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.local>
2017-05-23 17:39:56 +0200

    Ora e' in material design

--- application/view/admin/login.php
+++ application/view/admin/login.php
@@ -2,21 +2,27 @@
 <html>
 <head>
 	<title><?php echo $title; ?></title>
-	<link rel="stylesheet" href="<?php echo URL; ?>css/pure-min.css">
-	<link rel="stylesheet" href="<?php echo URL; ?>css/style.css">
-	<link rel="stylesheet" href="<?php echo URL; ?>css/grids-responsive-min.css">
-	<link rel="stylesheet" href="<?php echo URL; ?>css/font-awesome/css/font-awesome.min.css" type="text/css">
+	<link rel="stylesheet" href="<?php echo URL; ?>css/materialize.css">
+	<script type="text/javascript" src="<?php echo URL; ?>js/jquery.js"></script>
+	<script type="text/javascript" src="<?php echo URL; ?>js/materialize.js"></script>
 </head>
 <body>
-	<div class="container">
-		<form class="pure-form contenitore" method="POST" action="<?php echo URL; ?>admin/login">
-		    <fieldset class="pure-group">
-		    	<input type="text" class="pure-input-1-2" placeholder="Email" name="email_admin">
-		        <input type="password" class="pure-input-1-2" placeholder="Password" name="password_admin">
-		    </fieldset>
-		    <button type="submit" class="pure-button pure-input-1-2 pure-button-primary" name="login">Login</button>
-		    <br>
+	<div class="row">
+		<form class="col s12" method="POST" action="<?php echo URL; ?>admin/login">
+			<div class="row">
+				<div class="input-field col s6">
+					<input type="text" name="email_admin" id="email_admin" placeholder="Email" class="validate">
+					<label for="email_admin">Email admin</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s6">
+					<input type="password" name="password_admin" id="password_admin" placeholder="Password" class="validate">
+					<label for="password_admin">Password</label>
+				</div>
+			</div>
+			<button class="btn waves-effect waves-light" type="submit" name="login">Login</button>
 		</form>
-	</div>
+	</div>	
 </body>
 </html>
