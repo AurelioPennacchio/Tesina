e309fa53a71868681730516535502f7208863044
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.local>
2017-05-23 18:14:54 +0200

    Ora è completamente material

--- application/view/admin/menu.php
+++ application/view/admin/menu.php
@@ -2,26 +2,32 @@
 <html>
 <head>
 	<title>Admin Menu</title>
-	<meta charset="utf-8">
+	<link rel="stylesheet" href="<?php echo URL; ?>css/materialize.css">
 	<script type="text/javascript" src="<?php echo URL; ?>js/jquery.js"></script>
-	<link rel="stylesheet" href="<?php echo URL; ?>css/pure-min.css">
-	<link rel="stylesheet" href="<?php echo URL; ?>css/grids-responsive-min.css">
-	<link rel="stylesheet" href="<?php echo URL; ?>css/font-awesome/css/font-awesome.min.css" type="text/css">
+	<script type="text/javascript" src="<?php echo URL; ?>js/materialize.js"></script>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
-	<div class="pure-menu pure-menu-horizontal">
-		<ul class="pure-menu-list">
-			<li class="pure-menu-item">
-				<a href="<?php echo URL; ?>admin/aggiungiCibo" class="pure-button">Aggiungi cibo</a>
-			</li>
-			<li class="pure-menu-item">
-				<a href="<?php echo URL; ?>admin/eliminaCibo" class="pure-button">Elimina cibo</a>
-			</li>
-			<li class="pure-menu-item">
-				<a href="<?php echo URL; ?>admin/vediPrenotazioni" class="pure-button">Vedi prenotazioni</a>
-			</li>
-			<li class="pure-menu-item">
-				<a href="<?php echo URL; ?>admin/logout" class="pure-button ">Logout</a>
-			</li>
-		</ul>
+	<div>
+		<nav>
+			<div class="nav-wrapper">
+				<a href="#!" class="brand-logo">Admin</a>
+				<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+				<ul class="right hide-on-med-and-down">
+			    	<li><a href="<?php echo URL; ?>admin/vediPrenotazioni">Prenotazioni</a></li>
+			    	<li><a href="#">Statistiche</a></li>
+			    	<li><a href="#">Cibo</a></li>
+			    	<li><a href="<?php echo URL; ?>admin/logout">Logout</a></li>
+			    </ul>
+			    <ul class="side-nav" id="mobile-demo">
+			    	<li><a href="<?php echo URL; ?>admin/vediPrenotazioni">Prenotazioni</a></li>
+			    	<li><a href="#">Statistiche</a></li>
+			    	<li><a href="#">Cibo</a></li>
+			    	<li><a href="<?php echo URL; ?>admin/logout"</a></li>
+			    </ul>
+			</div>
+		</nav>
 	</div>
+	<script type="text/javascript">
+		$(".button-collapse").sideNav();
+	</script>
