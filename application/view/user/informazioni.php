1001944e13284d87e978e1f90bf79d6250b4e47d
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.homenet.telecomitalia.it>
2017-05-24 20:41:49 +0200

    Ora interfaccia responsive

--- application/view/user/informazioni.php
+++ application/view/user/informazioni.php
@@ -1,9 +1,29 @@
-			<div>
-				<h1>Ecco tutte le informazioni</h1>
-				<p>Nome: <?php echo $informazioni->nome; ?></p>
-				<p>Cognome: <?php echo $informazioni->cognome; ?></p>
-				<p>Email: <?php echo $informazioni->email; ?></p>
-				<p>Data di nascita: <?php echo $informazioni->data_nascita; ?></p>
+			
+			<div class="row">
+				<div class="col s12">
+					<h1 class="center-align">Le tue info</h1>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col s12">
+					<h5 class="center-align">Nome: <?php echo $informazioni->nome; ?></h5>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12">
+					<h5 class="center-align">Cognome: <?php echo $informazioni->cognome; ?></h5>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12">
+					<h5 class="center-align">Email: <?php echo $informazioni->email; ?></h5>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12">
+					<h5 class="center-align">Data di nascita: <?php echo $informazioni->data_nascita; ?></h5>
+				</div>
 			</div>
 	</body>
 </html>
