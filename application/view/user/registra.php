df276b0fe28fadd9155faf9feecc2ad4a4218b5e
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.homenet.telecomitalia.it>
2017-05-24 19:15:41 +0200

    Comparsa di messaggi dopo il tentativo di registrazione

--- application/view/user/registra.php
+++ application/view/user/registra.php
@@ -4,5 +4,30 @@
 	$email = $_POST['email'];
 	$data_nascita = $_POST['data_nascita'];
 	$password = $_POST['password'];
-	$temp = $this->model->addUser($nome, $cognome, $data_nascita, $email, $password);
+	if(empty($nome) || empty($cognome) || empty($data_nascita) || empty($email) || empty($password)){
+		$flag = false;
+	}
+	else{
+		$flag = true;
+		$temp = $this->model->addUser($nome, $cognome, $data_nascita, $email, $password);
+	}
 ?>
+<div class="row">
+	<div class="col s12">
+		<button class="btn waves-effect waves-light" name="login">
+			<?php
+				if($flag == false){
+					echo "Errore durante la registrazione";
+				}
+				else{
+					if($temp == false){
+						echo "Utente già registrato";
+					}
+					else{
+						echo "Registrazione avvenuta con successo";
+					}
+				}
+			?>
+		</button>
+	</div>
+</div>
