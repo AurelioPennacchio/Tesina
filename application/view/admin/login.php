e988eee947f232ba941a14cc54879662df5d6ab9
Aurelio <Sandra@MacBook-Air-di-Sandra.local>
2017-05-24 12:48:18 +0200

    Aggiunto file css

--- application/view/admin/login.php
+++ application/view/admin/login.php
@@ -5,6 +5,7 @@
 	<link rel="stylesheet" href="<?php echo URL; ?>css/materialize.css">
 	<script type="text/javascript" src="<?php echo URL; ?>js/jquery.js"></script>
 	<script type="text/javascript" src="<?php echo URL; ?>js/materialize.js"></script>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
@@ -13,12 +14,14 @@
 			<form class="col s12" method="POST" action="<?php echo URL; ?>admin/login">
 				<div class="row">
 					<div class="input-field col s12">
+						<i class="material-icons prefix">account_circle</i>
 						<input type="text" name="email_admin" id="email_admin" placeholder="Email" class="validate">
 						<label for="email_admin">Email admin</label>
 					</div>
 				</div>
 				<div class="row">
 					<div class="input-field col s12">
+						<i class="material-icons prefix">lock_outline</i>
 						<input type="password" name="password_admin" id="password_admin" placeholder="Password" class="validate">
 						<label for="password_admin">Password</label>
 					</div>
