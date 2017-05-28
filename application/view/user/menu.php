7f0cfce2ba58e984eb4449eda5e1cb69489ac7f9
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.local>
2017-05-28 10:27:21 +0200

    Tolta la dicitura Admin menu

--- application/view/user/menu.php
+++ application/view/user/menu.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Admin Menu</title>
+    <title></title>
     <link rel="stylesheet" href="<?php echo URL; ?>css/materialize.css">
     <script type="text/javascript" src="<?php echo URL; ?>js/jquery.js"></script>
     <script type="text/javascript" src="<?php echo URL; ?>js/materialize.js"></script>
