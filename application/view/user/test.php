6c6c28b90e6324286179d97297276b5645f4c7cc
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.local>
2017-05-28 10:26:50 +0200

    Apportate delle modifiche

--- application/view/user/test.php
+++ application/view/user/test.php
@@ -1,11 +1,11 @@
 <?php 
-	//var_dump($prova2);
+	var_dump($prova2);
 	$i = 0;
-	
+	/*
 	while($i < count($prova2)-2) {
 		echo $prova2[$i]->Cibo . '<br>';
 		echo $prova2[$i+1]->Cibo . '<br>';
 		echo $prova2[$i+2]->Cibo . '<br>';
 		$i+=3;
-	}
+	}*/
 ?>
