1f4c8e27d95f3bed6dc212f818229cc28b0bffd0
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.local>
2017-05-28 09:46:52 +0200

    Prenotazione effettuate con la nuova funzione

--- application/view/user/prenota.php
+++ application/view/user/prenota.php
@@ -1,8 +1,19 @@
 <?php
 	session_start();
-	$id_primo_piatto = $_POST['primo_piatto'];
-	$id_secondo_piatto = $_POST['secondo_piatto'];
+	$id_primo = $_POST['primo_piatto'];
+	$id_secondo = $_POST['secondo_piatto'];
 	$id_bibita = $_POST['bibita'];
 	$id_utente = $_SESSION['id'];
-	$this->model->addPrenotazione($id_primo_piatto,$id_secondo_piatto,$id_bibita,$id_utente);
+	//$this->model->addPrenotazione($id_primo_piatto,$id_secondo_piatto,$id_bibita,$id_utente);
+	$prenotazione = $this->model->addPrenotazioneCompleta($id_utente, $id_primo, $id_secondo, $id_bibita);
 ?>
+<div class="col s12">
+	<?php
+		if($prenotazione){
+			echo "<a class=\"waves-effect waves-light btn\">Prenotato!</a>";
+		}
+		else{
+			echo "<a class=\"waves-effect waves-light btn\">Prenotazione gia fatta!</a>";
+		}
+	?>
+</div>
