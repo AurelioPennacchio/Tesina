42b845235cf420c8fd3331a19343a14745368f7a
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.local>
2017-05-28 10:27:09 +0200

    Ora si vedono le prenotazioni che sono state effettuate

--- application/view/user/prenotazioni.php
+++ application/view/user/prenotazioni.php
@@ -1,4 +1,36 @@
 
-			<h1>Ecco tutte le prenotazioni</h1>
+			<div class="row">
+				<div class="col s12">
+					<h1 class="center-align">Prenotazioni effettuate</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12 m6 offset-m3 ">
+					<table class="bordered responsive-table centered">
+						<thead>
+							<tr>
+								<th>Data</th>
+								<th>Primo piatto</th>
+								<th>Secondo piatto</th>
+								<th>Bibita</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+								$i = 0;
+								while($i < count($prenotazioni)-2) {
+									echo "<tr>";
+									echo  "<td>" . $prenotazioni[$i]->data . "</td>";
+									echo  "<td>" . $prenotazioni[$i]->Cibo . "</td>";
+									echo  "<td>" . $prenotazioni[$i+1]->Cibo . "</td>";
+									echo  "<td>" . $prenotazioni[$i+2]->Cibo . "</td>";
+									$i+=3;
+									echo "</tr>";
+								}
+							?>
+						</tbody>
+					</table>
+				</div>
+			</div>
 	</body>
 </html>
