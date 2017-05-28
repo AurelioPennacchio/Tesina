0729df1d0aaf6e0839077616f416b72ce385957b
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.homenet.telecomitalia.it>
2017-05-28 14:56:31 +0200

    Moficata l'interfaccia utente

--- application/view/admin/login.php
+++ application/view/admin/login.php
@@ -11,28 +11,33 @@
 <body>
 	<div class="container">
 		<div class="row">
-			<form class="col s12" method="POST" action="<?php echo URL; ?>admin/login">
-				<div class="row">
-					<div class="input-field col s12">
-						<i class="material-icons prefix">account_circle</i>
-						<input type="text" name="email_admin" id="email_admin" placeholder="Email" class="validate">
-						<label for="email_admin">Email admin</label>
-					</div>
+			<div class="card blue-gray darken-1">
+				<div class="card-content black-text">
+					<span class="card-title center-align">Admin login</span>
+					<form method="POST" action="<?php echo URL; ?>admin/login">
+						<div class="row">
+							<div class="input-field col s12">
+								<i class="material-icons prefix">account_circle</i>
+								<input type="text" name="email_admin" id="email_admin" placeholder="Email" class="validate">
+								<label for="email_admin">Email admin</label>
+							</div>
+						</div>
+						<div class="row">
+							<div class="input-field col s12">
+								<i class="material-icons prefix">lock_outline</i>
+								<input type="password" name="password_admin" id="password_admin" placeholder="Password" class="validate">
+								<label for="password_admin">Password</label>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col m12 s12">
+								<p class="center-align"><button class="btn waves-effect waves-light" type="submit" name="login">Login</button></p>
+							</div>
+						</div>
+					</form>
 				</div>
-				<div class="row">
-					<div class="input-field col s12">
-						<i class="material-icons prefix">lock_outline</i>
-						<input type="password" name="password_admin" id="password_admin" placeholder="Password" class="validate">
-						<label for="password_admin">Password</label>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col s6 offset-s4">
-						<button class="btn waves-effect waves-light" type="submit" name="login">Login</button>
-					</div>
-				</div>
-			</form>
+			</div>
 		</div>
-	</div>	
+	</div>
 </body>
 </html>
